1133cc6540720d89e4ad58c5fe1c7a340524cd4f
VerigoOo <atilay_yoruk@outlook.com>
2023-06-21 23:15:19 +0300

    Com

---
+++ combination.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class combination {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("N değerini giriniz: ");
+            int n = input.nextInt();
+            System.out.print("R değerini giriniz: ");
+            int r = input.nextInt();
+            int nFaktoriyel = 1;
+            int rFaktoriyel = 1;
+            int nMinusRFaktoriyel = 1;
+            for (int i = 1; i <= n; i++) {
+                nFaktoriyel *= i;
+                if (i <= r) {
+                    rFaktoriyel *= i;
+                }
+                if (i <= n - r) {
+                    nMinusRFaktoriyel *= i;
+                }
+            }
+            int c = nFaktoriyel / (rFaktoriyel * nMinusRFaktoriyel);
+            System.out.println("C(" + n + "," + r + ") = " + c);
+        }
+    }
+}
