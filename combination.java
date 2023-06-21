ef4d48479b767321f9f3b2370d5beea94bf5cc2c
VerigoOo <atilay_yoruk@outlook.com>
2023-06-21 23:16:17 +0300

    Com

--- combination.java
+++ combination.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class combination {
+public class Combination {
     public static void main(String[] args) {
         try (Scanner input = new Scanner(System.in)) {
             System.out.print("N değerini giriniz: ");
